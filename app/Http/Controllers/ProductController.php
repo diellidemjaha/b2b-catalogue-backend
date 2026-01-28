e2e9cbe390ce616f9e72435fe0c6dc75efeedd6d
Dielli Demjaha <diellidemjaha@gmail.com>
2026-01-28 14:55:24 +0100

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,4 +16,25 @@ class ProductController extends Controller
     {
         return Product::findOrFail($id);
     }
+      public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'price' => 'required|numeric',
+            'description' => 'nullable|string',
+            'image_url' => 'nullable|string',
+            'sku' => 'nullable|string',
+        ]);
+
+        $product = Product::create([
+            'name' => $data['name'],
+            'price' => $data['price'],
+            'description' => $data['description'] ?? null,
+            'image_url' => $data['image_url'] ?? null,
+            'sku' => $data['sku'] ?? null,
+            'is_active' => true,
+        ]);
+
+        return response()->json($product, 201);
+    }
 }
